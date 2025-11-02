2c2aaebf94fd958d395f9464637780a5877a0f7f
Awish Mu <61050376+awishmu@users.noreply.github.com>
2025-11-02 21:56:35 +0800

    Add class to Minuman link for full width mobile menu92
    Add class to Minuman link for full width mobile menu92

--- components/shared/element/mobile-menu.tsx
+++ components/shared/element/mobile-menu.tsx
@@ -88,7 +88,7 @@ export function MobileMenu() {
 							</li>
 							<li id="menu-item" className="w-full p-3 border-b-2">
 								<div id="menu_content" className="m-auto">
-									<Link href="#" className="">Minuman</Link>
+									<Link href="#" className="w-full">Minuman</Link>
 									<Popover className="w-screen">
 									  <PopoverTrigger asChild>
 										<Button variant="outline" className="absolute ml-4"><ChevronDown className="fill-night"/></Button>
