e9885e2b2a1361c7ccf16aa54bd9030ebbb3bf6c
Awish Mu <61050376+awishmu@users.noreply.github.com>
2025-11-15 11:09:47 +0800

    Fix label positioning for message textarea
    className

--- app/demo-29/contact/page.tsx
+++ app/demo-29/contact/page.tsx
@@ -88,7 +88,7 @@ export default function ContactUs() {
 							<textarea id="message" rows={4} placeholder=" "  name="message"
 								 className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-200"
 							/>
-							<label htmlFor="message" className="placeholder"
+							<label htmlFor="message" 
 								className="absolute left-2 -top-4 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 transition-all peer-focus:-top-4 peer-focus:text-gray-600 peer-focus:text-sm"
 							>Pesan/Pertanyaan<span className="required"> *</span></label>
 						</div>
