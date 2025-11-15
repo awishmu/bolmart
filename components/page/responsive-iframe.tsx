4b88934e53e1994fb82f4061ddaade87102468e0
Awish Dot <awishdotid@gmail.com>
2025-11-15 14:34:07 +0800

    kerja contact 11/15/2025 2:34pm

--- components/page/responsive-iframe.tsx
+++ components/page/responsive-iframe.tsx
@@ -1,10 +1,6 @@
-// components/ResponsiveIframe.js
 import React from "react";
-<<<<<<< HEAD
 // import styles from "@/styles/map-frame.module.css"
-=======
-import styles from "./map-frame.module.css"
->>>>>>> ff2ab5fe08907587712ac615a88ff5bb9f44453a
+
 export default function ResponsiveIframe({ src, title }) {
   if (!src || typeof src !== "string") {
     console.error("Invalid iframe src provided");
@@ -12,19 +8,11 @@ export default function ResponsiveIframe({ src, title }) {
   }
 
   return (
-<<<<<<< HEAD
     <div className="w-full relative h-[40rem] mx-auto">
       <iframe
         src={src}
         title={title || "Embedded content"}
         className="w-full max-w-[64rem] h-[40rem] absolute mx-auto top-0 "
-=======
-    <div className={styles.wrapper}>
-      <iframe
-        src={src}
-        title={title || "Embedded content"}
-        className={styles.iframemap}
->>>>>>> ff2ab5fe08907587712ac615a88ff5bb9f44453a
         frameBorder="0"
         allowFullScreen
         loading="lazy" // Improves performance
@@ -33,8 +21,4 @@ export default function ResponsiveIframe({ src, title }) {
       />
     </div>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ff2ab5fe08907587712ac615a88ff5bb9f44453a
