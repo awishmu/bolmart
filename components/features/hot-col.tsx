dcb9b287d7b63cc7da2699b7780b4134dfa940fb
Awish Dot <awishdotid@gmail.com>
2025-11-19 08:29:26 +0800

    kerja deal month 11/19/2025 8:29am

---
+++ components/features/hot-col.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export function SpecialRow() {
+	return (
+	
+		<div>
+			<h1>Deals Hot Of The Day</h1>
+			<div>
+				<div>Automatic</div>
+				<div>Charming</div>
+			</div>
+			
+		</div>
+	)
+}
---
+++ components/features/special-row.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export function SpecialRow() {
+	return (
+	
+		<div>
+			<image bg />
+			<div>
+				<h2>Today's Special</h2>
+				<h1>Trneding Accessories Collection</h1>
+			</div>
+			
+		</div>
+	)
+}
