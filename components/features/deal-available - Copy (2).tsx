2dfa752321a6b8c82a827ff7e0eaa2ac476c803f
Awish Dot <awishdotid@gmail.com>
2025-11-19 08:10:46 +0800

    kerja deal month 11/19/2025 8:10am

--- components/features/deal-available - Copy (2).tsx
+++ components/features/deal-available - Copy (2).tsx
@@ -8,30 +8,22 @@ export function DealAvailable () {
 // width 70%
 	return (
 		<div className="w-full flex flex-col md:flex-row gap-1">
-			<div className="relative w-full flex md:w-[25%] h-[32rem] rounded-md my-2 bg-linear-to-b from-zinc-900 to-zinc-300">
-				<div className="absolute top-[10%] w-full text-center flex flex-col mx-auto">
-					<h3 className="text-amber-400 text-xl uppercase">Hot This Month</h3>
-					<div>
-						<h2 className="text-white text-2xl font-medium">Save For Extra</h2>
-						<h2 className="text-lime-400 text-2xl font-bold">$15 Per Order</h2>
-					</div>
-				</div>
-			</div>
-			<div className="w-full flex flex-col md:w-[75%]">
-				<div className="w-full p-2"><h2 className="text-zinc-800 text-2xl font-medium">Deal of The Month</h2></div>
+			<div className="w-full flex md:w-[25%] h-[24rem] border-2 bg-yellow-500">Banner Light Promo</div>
+			<div className="w-full flex flex-col md:w-[75%] border-2 border-green-800">
+				<div className="w-full p-2"><h2>Deal of the month products</h2></div>
 				<div className="flex flex-wrap lg:flex-nowrap">
 					{/* Item Product 1 */}										
-					<div className="overflow-hidden relative w-[14rem] m-2 p-2 bg-zinc-50 rounded-md">						
-						<div className="relative w-[211px] h-[385px] flex flex-col justify-center shadow-md rounded-xl duration-500">
-							<div className="block z-18 absolute top-0 w-[40%] bg-amber-400 text-white p-1 m-1 rounded-lg">14% Off</div>
-							<Link href="#" className="z-8 absolute top-6 flex justify-end w-full h-[228px]">
+					<div className="overflow-hidden relative w-[14rem] border-2 m-2 p-2 bg-lime-200">						
+						<div className="relative w-[211px] h-[385px] flex flex-col justify-center bg-white shadow-md rounded-xl duration-500 border-2 border-lime-200">
+							<div className="block z-18 absolute top-0 w-[40%] bg-amber-400 text-white  p-1 m-1 rounded-lg">14% Off</div>
+							<Link href="#" className="z-8 absolute top-6 bg-red-500 flex justify-end w-full h-[228px] p-2">
 								<Image  
 									src="/Native_Chicken_Rustic_Studio_Shot.png" alt="" 
 									width={100} height={100}
 									style={{ objectFit: 'fill', borderRadius: '10px', objectPosition: 'top', width:'200px', height: '228px'}}
 								/>
 							</Link>
-							<div className="absolute -bottom-16 z-18 w-full mx-auto flex flex-col bg-lime-200/30 p-4 duration-400 translate-y-1 hover:-translate-y-12 hover:scale-103 hover:shadow-xl">
+							<div className="z-18 w-full mx-auto flex flex-col bg-lime-200/30 p-4 duration-400 translate-y-8 hover:-translate-y-0 hover:scale-103 hover:shadow-xl">
  								<div className="flex justify-between">
 									<h3 className="text-xl font-medium">Ayam Kampung Frozen Segar</h3>
 									<div className="px-1 text-white rounded-md bg-lime-400 hover:bg-lime-600 text-center flex items-center justify-center">
@@ -49,7 +41,7 @@ export function DealAvailable () {
 									</div>
 								</div>
 								<div className="w-[97%] h-1 mx-auto bg-lime-600"> </div>
-								<div className="text-zinc-600 flex justify-between mb-3 font-light">
+								<div className="text-zinc-600 flex justify-between mb-2 font-light">
 									<p className="">Available: <span>173</span></p>
 									<p className="">Sold: <span>4</span></p>
 								</div>								
@@ -62,17 +54,17 @@ export function DealAvailable () {
 					</div>
 					{/** End Item Product 1 */}		
 					{/* Item Product 2 */}												
-					<div className="overflow-hidden relative w-[14rem] m-2 p-2 bg-zinc-50 rounded-md">						
-						<div className="relative w-[211px] h-[385px] flex flex-col justify-center shadow-md rounded-xl duration-500">
-							<div className="block z-18 absolute top-0 w-[40%] bg-amber-400 text-white p-1 m-1 rounded-lg">14% Off</div>
-							<Link href="#" className="z-8 absolute top-6 flex justify-end w-full h-[228px]">
-								<Image  
+					<div className="overflow-hidden relative w-[14rem] border-2 m-2 p-2 bg-lime-200">						
+						<div className="relative w-[211px] h-[385px] flex flex-col justify-center bg-white shadow-md rounded-xl duration-500 border-2 border-lime-200">
+							<div className="block w-[40%] bg-amber-400 text-white  p-1 m-1 rounded-lg">14% Off</div>
+							<Link href="#" className="z-9 relative top-0 flex justify-end w-full h-[228px] p-2">
+								<Image 
 									src="/Farm_Fresh_Egg_Collection.png" alt="" 
-									width={100} height={100}
-									style={{ objectFit: 'fill', borderRadius: '10px', objectPosition: 'top', width:'200px', height: '228px'}}
+									fill 
+									style={{ objectFit: 'fill', borderRadius: '10px', objectPosition: 'right'}}
 								/>
 							</Link>
-							<div className="absolute -bottom-16 z-18 w-full mx-auto flex flex-col bg-lime-200/30 p-4 duration-400 translate-y-1 hover:-translate-y-12 hover:scale-103 hover:shadow-xl">
+							<div className="z-9 w-full mx-auto flex flex-col bg-lime-200/30 p-4 duration-400 translate-y-16 hover:-translate-y-0 hover:scale-103 hover:shadow-xl">
  								<div className="flex justify-between">
 									<h3 className="text-xl font-medium">Telur Ayam Kampung Fertil</h3>
 									<div className="px-1 text-white rounded-md bg-lime-400 hover:bg-lime-600 text-center flex items-center justify-center">
@@ -90,7 +82,7 @@ export function DealAvailable () {
 									</div>
 								</div>
 								<div className="w-[97%] h-1 mx-auto bg-lime-600"> </div>
-								<div className="text-zinc-600 flex justify-between mb-3 font-light">
+								<div className="text-zinc-600 flex justify-between mb-2 font-light">
 									<p className="">Available: <span>173</span></p>
 									<p className="">Sold: <span>4</span></p>
 								</div>								
@@ -103,17 +95,17 @@ export function DealAvailable () {
 					</div>
 					{/** End Item Product 2 */}	
 					{/* Item Product 3 */}
-					<div className="overflow-hidden relative w-[14rem] m-2 p-2 bg-zinc-50 rounded-md">						
-						<div className="relative w-[211px] h-[385px] flex flex-col justify-center shadow-md rounded-xl duration-500">
-							<div className="block z-18 absolute top-0 w-[40%] bg-amber-400 text-white p-1 m-1 rounded-lg">14% Off</div>
-							<Link href="#" className="z-8 absolute top-6 flex justify-end w-full h-[228px]">
-								<Image  
+					<div className="overflow-hidden relative w-[14rem] border-2 m-2 p-2 bg-lime-200">						
+						<div className="relative w-[211px] h-[385px] flex flex-col justify-center bg-white shadow-md rounded-xl duration-500 border-2 border-lime-200">
+							<div className="block w-[40%] bg-amber-400 text-white  p-1 m-1 rounded-lg">14% Off</div>
+							<Link href="#" className="z-9 relative top-0 flex justify-end w-full h-[228px] p-2">
+								<Image 
 									src="/Fluffy_Chicks_Golden_Straw.png" alt="" 
-									width={100} height={100}
-									style={{ objectFit: 'fill', borderRadius: '10px', objectPosition: 'top', width:'200px', height: '228px'}}
+									fill 
+									style={{ objectFit: 'fill', borderRadius: '10px', objectPosition: 'right'}}
 								/>
 							</Link>
-							<div className="absolute -bottom-16 z-18 w-full mx-auto flex flex-col bg-lime-200/30 p-4 duration-400 translate-y-1 hover:-translate-y-12 hover:scale-103 hover:shadow-xl">
+							<div className="z-9 w-full mx-auto flex flex-col bg-lime-200/30 p-4 duration-400 translate-y-16 hover:-translate-y-0 hover:scale-103 hover:shadow-xl">
  								<div className="flex justify-between">
 									<h3 className="text-xl font-medium">Anak Ayam Kampung Asli</h3>
 									<div className="px-1 text-white rounded-md bg-lime-400 hover:bg-lime-600 text-center flex items-center justify-center">
@@ -131,7 +123,7 @@ export function DealAvailable () {
 									</div>
 								</div>
 								<div className="w-[97%] h-1 mx-auto bg-lime-600"> </div>
-								<div className="text-zinc-600 flex justify-between mb-3 font-light">
+								<div className="text-zinc-600 flex justify-between mb-2 font-light">
 									<p className="">Available: <span>173</span></p>
 									<p className="">Sold: <span>4</span></p>
 								</div>								
@@ -144,19 +136,19 @@ export function DealAvailable () {
 					</div>
 					{/** End Item Product 3 */}	 
 					{/* Item Product 4 */}
-					<div className="overflow-hidden relative w-[14rem] m-2 p-2 bg-zinc-50 rounded-md">						
-						<div className="relative w-[211px] h-[385px] flex flex-col justify-center shadow-md rounded-xl duration-500">
-							<div className="block z-18 absolute top-0 w-[40%] bg-amber-400 text-white p-1 m-1 rounded-lg">14% Off</div>
-							<Link href="#" className="z-8 absolute top-6 flex justify-end w-full h-[228px]">
-								<Image  
+					<div className="overflow-hidden relative w-[14rem] border-2 m-2 p-2 bg-lime-200">						
+						<div className="relative w-[211px] h-[385px] flex flex-col justify-center bg-white shadow-md rounded-xl duration-500 border-2 border-lime-200">
+							<div className="block w-[40%] bg-amber-400 text-white p-1 m-1 rounded-lg">14% Off</div>
+							<Link href="#" className="z-9 relative top-0 flex justify-end w-full h-[228px] p-2">
+								<Image 
 									src="/Majestic_Rooster_Sunrise_Portrait.png" alt="" 
-									width={100} height={100}
-									style={{ objectFit: 'fill', borderRadius: '10px', objectPosition: 'top', width:'200px', height: '228px'}}
+									fill 
+									style={{ objectFit: 'fill', borderRadius: '10px', objectPosition: 'right'}}
 								/>
 							</Link>
-							<div className="absolute -bottom-16 z-18 w-full mx-auto flex flex-col bg-lime-200/30 p-4 duration-400 translate-y-1 hover:-translate-y-12 hover:scale-103 hover:shadow-xl">
+							<div className="z-9 w-full mx-auto flex flex-col bg-lime-200/30 p-4 duration-400 translate-y-16 hover:-translate-y-0 hover:scale-103 hover:shadow-xl">
  								<div className="flex justify-between">
-									<h3 className="text-xl font-medium">Indukan Ayam Kampung Asli</h3>
+									<h3 className="text-xl font-medium">Indukan Ayam Kampung</h3>
 									<div className="px-1 text-white rounded-md bg-lime-400 hover:bg-lime-600 text-center flex items-center justify-center">
 										<Link href="#"><button className="px-2 py-1">Add</button></Link>
 									</div>
@@ -172,7 +164,7 @@ export function DealAvailable () {
 									</div>
 								</div>
 								<div className="w-[97%] h-1 mx-auto bg-lime-600"> </div>
-								<div className="text-zinc-600 flex justify-between mb-3 font-light">
+								<div className="text-zinc-600 flex justify-between mb-2 font-light">
 									<p className="">Available: <span>173</span></p>
 									<p className="">Sold: <span>4</span></p>
 								</div>								
