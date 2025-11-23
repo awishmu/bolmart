b8d0714ef0441768bc47a050e483626ee7a64801
Awish Dot <awishdotid@gmail.com>
2025-11-23 22:15:29 +0800

    item shop  11/23/2025 10:15pm

--- app/demo-29/page.tsx
+++ app/demo-29/page.tsx
@@ -1,10 +1,20 @@
+
+import items from "@/db/items.json";
+
+
 export default function Page() {
+	
+	
+	
   return (
   <>
 	 
 		<h1 className="text-3xl font-bold underline bg-blue-300">
 		  Home Demo-29 Page
 		</h1>
+		{items.itemsGrid.map((item) => (
+			<div key={item.id}>{item.title}</div>
+		))}
     </>
   )
 }	
