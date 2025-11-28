00e5f5397b20cff29e2deb90b9f07486ac89bbe3
Awish Mu <61050376+awishmu@users.noreply.github.com>
2025-11-28 21:11:24 +0800

    Add remote image pattern for bolmart
    Add remote image pattern for bolmart

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@
 const nextConfig = {
 	/** output: 'export', */
 	basePath: '',
+	images: {
+	    remotePatterns: [
+	      {
+	        protocol: 'https',
+	        hostname: 'awishmu.github.io/bolmart',
+	        port: '',
+	        pathname: '/**',
+	      },
+		]
+	  },
+	
 }
 
 module.exports = nextConfig
