ccef798e47e669dcaef435a5885e4330dc9b1687
Awish Mu <61050376+awishmu@users.noreply.github.com>
2025-11-28 21:24:40 +0800

    Add new image host configurations to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,18 @@ const nextConfig = {
 	        protocol: 'https',
 	        hostname: 'awishmu.github.io/bolmart',
 	      },
+		  {
+	        protocol: 'https',
+	        hostname: 'media.istockphoto.com',
+	        port: '',
+	        pathname: '/**',
+	      },
+		  {
+	        protocol: 'https',
+	        hostname: 'cdn.pixabay.com',
+	        port: '',
+	        pathname: '/**',
+	      },
 		]
 	  },
 	
